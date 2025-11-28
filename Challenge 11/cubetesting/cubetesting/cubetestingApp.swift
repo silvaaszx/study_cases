1d8c3fce0a09e834bfe87c5fc155e3eaaebd8039
Matheus Silva <124085006+silvaaszx@users.noreply.github.com>
2025-11-28 14:43:13 -0300

    Add files via upload

---
+++ Challenge 11/cubetesting/cubetesting/cubetestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  cubetestingApp.swift
+//  cubetesting
+//
+//  Created by Matheus Silva on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct cubetestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
