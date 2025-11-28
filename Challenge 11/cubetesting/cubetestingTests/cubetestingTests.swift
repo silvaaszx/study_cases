7ffeb010d248f7726fcbf2b13cb5883ffa0354c7
Matheus Silva <124085006+silvaaszx@users.noreply.github.com>
2025-11-28 14:44:59 -0300

    Add files via upload

---
+++ Challenge 11/cubetesting/cubetestingTests/cubetestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  cubetestingTests.swift
+//  cubetestingTests
+//
+//  Created by Matheus Silva on 27/11/25.
+//
+
+import Testing
+@testable import cubetesting
+
+struct cubetestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Challenge 11/cubetesting/cubetestingUITests/cubetestingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  cubetestingUITestsLaunchTests.swift
+//  cubetestingUITests
+//
+//  Created by Matheus Silva on 27/11/25.
+//
+
+import XCTest
+
+final class cubetestingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
